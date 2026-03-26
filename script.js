2c1b6a38c391c33f29b58536092e2d66194e675e
Memo5561 <mehmetkose2807@gmail.com>
2026-03-26 14:10:43 +0300

    script.js erstellen

---
+++ script.js
@@ -0,0 +1,18 @@
+const animatedElements = document.querySelectorAll(".animate-on-scroll");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  }
+);
+
+animatedElements.forEach((element) => {
+  observer.observe(element);
+});
